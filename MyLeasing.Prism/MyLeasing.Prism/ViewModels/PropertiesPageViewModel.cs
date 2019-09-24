00b6fd870b329b4d0f46f2d18fa4f0ec474f9061
MATEO ROLDAN ZAPATA <mateoroldan183493@correo.itm.edu.co>
2019-09-24 01:26:26 -0500

    List Properties

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using MyLeasing.Common.Models;
 using Prism.Navigation;
+using System.Collections.ObjectModel;
 
 namespace MyLeasing.Prism.ViewModels
 {
     public class PropertiesPageViewModel : ViewModelBase
     {
-        private OwnerResponse _owner; 
+        private OwnerResponse _owner;
+        private ObservableCollection<PropertyResponse> _properties;
 
         public PropertiesPageViewModel(
             INavigationService navigationService) : base(navigationService)
@@ -13,14 +15,21 @@ namespace MyLeasing.Prism.ViewModels
             Title = "Properties";
         }
 
+        public ObservableCollection<PropertyResponse> Properties
+        {
+            get => _properties;
+            set => SetProperty(ref _properties, value);
+        }
+
         public override void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
 
-            if(parameters.ContainsKey("owner"))
+            if (parameters.ContainsKey("owner"))
             {
                 _owner = parameters.GetValue<OwnerResponse>("owner");
-               Title = $"Properties of: { _owner.FullName}";
+                Title = $"Properties of: { _owner.FullName}";
+                Properties = new ObservableCollection<PropertyResponse>(_owner.Properties);
             }
         }
     }
